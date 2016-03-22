328b44e756a563576a2454a947ea6869eb86766d
Mitruxa <52301323senfn>
2016-03-22 20:20:53 +0200

    Initial commit.

---
+++ src/main/java/com/mitrofanov/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.mitrofanov.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BaseController{
+    @RequestMapping(value="/")
+    public String velcome(Model model){
+        return "index";
+    }
+
+
+
+
+}
