08b0615c44e737c882b5131c8c214ac768c5f8cb
John <Jhon@John.com>
2016-04-02 14:27:49 +0300

    Add selenium with verification test (!!!machine specific Firefox driver!!!).

--- src/main/test/integration/IndexPageTest.java
+++ src/main/test/integration/IndexPageTest.java
@@ -1,6 +1,24 @@
 package integration;
 
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+
 public class IndexPageTest {
 
+    @Test
+    public void startWebDriver() {
+        WebDriver driver = new FirefoxDriver(new FirefoxBinary(new File("E:/Andrew/Firefox/firefox.exe")),  new FirefoxProfile());
+        driver.navigate().to("http://seleniumsimplified.com");
+        assertTrue(driver.getTitle().startsWith("Selenium Simplified"));
 
+        driver.close();
+        driver.quit();
+    }
 }
