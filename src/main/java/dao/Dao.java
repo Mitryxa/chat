bb7457e1dbd636e466802e861d741172ba512c0e
Mitruxa <52301323senfn>
2016-04-06 23:29:16 +0300

    Implement DaoMethod.

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -3,5 +3,10 @@ package dao;
 /**
  * Created by Mitruxa on 03.04.2016.
  */
+
 public class Dao {
+    public static String saveName(String expected) {
+        System.out.println(expected);
+        return expected;
+    }
 }
